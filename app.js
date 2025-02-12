51176679d2f7548cf1e2d98c91ce4c7a773ae2a5
Cecilia Solis <zelgadys_31@hotmail.com>
2025-02-11 20:32:01 -0600

    Funcion para hacer sorteo

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 //Array para la lista de amigos
-let amigos = [];
-
-console.log (typeof (capturaAmigo));
+let amigos =[];
 
 //funcion para que el usuario agregue a un amigo
 function agregarAmigo(){
     let capturaAmigo = document.getElementById("amigo");
-    let nombresDeAmigos = capturaAmigo.ariaValueMax.trim();
-    console.log (capturaAmigo)
-    console.log (typeof (capturaAmigo));
+    let nombresDeAmigos = capturaAmigo.value.trim();
 
-    if (capturaAmigo="") {
+    if (nombresDeAmigos ==="") {
     alert ("Por favor, inserte un nombre.");
     return;
 }
+
 //Comprobar que el nombre no este duplicado
 if (amigos.includes(nombresDeAmigos)) {
-    alert(`Se ha duplicado el nombre ${nombresDeAmigos} de tu lista de Amigos`)
+    alert(`El nombree ${nombresDeAmigos} ya está en tu lista de amigos.`);
     return;
 }
 
@@ -26,7 +23,7 @@ if (amigos.includes(nombresDeAmigos)) {
 amigos.push(nombresDeAmigos);
 
 //Limpiar la Caja de los nombnres de amigos
-capturaAmigo.ariaValu = "";
+capturaAmigo.value = "";
 
 // Muestra la lista actualizada de amigos en el HTML
 listaActualizada();
@@ -35,18 +32,15 @@ listaActualizada();
 
 // funcion para actualizar la lista de amigos
 function listaActualizada() {
-    let elementoHTML = document.querySelector("listaAmigos");
-    elementoHTML.innerHTML = "";
+    let listaAmigos = document.querySelector("#listaAmigos");
+    listaAmigos.innerHTML = "";
 
     //Interar sobre la lista con for  y craer elementos li
     for (let index = 0; index < amigos.length; index++) {
         const li = document.createElement("li");
-        li.textContent =amigos(index);
-        amigos.appendChild(li);
-
-        
+        li.textContent = amigos[index];
+        listaAmigos.appendChild(li);
     }
-
 }
 
 
@@ -54,12 +48,17 @@ function listaActualizada() {
 function sortearAmigo() {
  //validar que no este repetido el nombre
  if (amigos.length === 0) {
-    alert("Y no me queje")//Interar sobre
+    alert("No se ha introducido ningun amigo"); //Interar sobre la lista de amigos
+    return;
  }
+ // Crear un indice aleatorio con Math de la lista
+ let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+ // Sortear nombre de amigo
+ let nombreDeAmigoSorteado = amigos [indiceAleatorio];
+
+ //Mostrar Resultado
+ let resultado = document.getElementById("resultado");
+ resultado.innerHTML = (`Amigo Secreto: ${nombreDeAmigoSorteado}`);
+
 }
-
-
-console.log (amigos[amigos.length]);
-
-
-
